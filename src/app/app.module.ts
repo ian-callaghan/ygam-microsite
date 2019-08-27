6846fa0fdafb8b96d3dfaa7de4c07610ac73a1f6
ian <ian.t.callaghan@gmail.com>
2019-08-28 00:37:13 +0100

    templating system and first few components styled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { TitleBannerComponent } from "./title-banner/title-banner.component";
+import { DateBarComponent } from "./date-bar/date-bar.component";
+import { IntroBlockComponent } from "./intro-block/intro-block.component";
+import { HttpClientModule } from "@angular/common/http";
+import { ComponentsModule } from "./components.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
